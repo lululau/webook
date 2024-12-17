f711ccfbafadb08550f20863a4a70f4afdeb1c66
Liu Xiang <liuxiang921@gmail.com>
2024-12-17 16:55:02 +0800

    add js file and wait seconds

--- webook/web.py
+++ webook/web.py
@@ -1,7 +1,7 @@
 from optparse import Values
 from webook import selectors
 from webook.urls import URL
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError
 
 def save_as_pdf(links: list[URL], tempdir: str, options: Values) -> list[str]:
     pdfs = []
@@ -10,13 +10,23 @@ def save_as_pdf(links: list[URL], tempdir: str, options: Values) -> list[str]:
         page = browser.new_page()
         for link in links:
             page_selectors = selectors.for_url(link.url, options.delete_element_file)
-            page.goto(link.url)
+            try:
+                page.goto(link.url, timeout=options.wait_seconds * 1000)
+            except TimeoutError:
+                if not options.ignore_wait_error:
+                    raise
             # page.wait_for_event('domcontentloaded')
             # remove elemtents
             for selector in page_selectors:
                 if selector:
                     for elem in page.query_selector_all(selector):
                         elem.evaluate('e => e.remove()')
+
+            if options.js_file:
+                with open(options.js_file, 'r') as f:
+                    js = f.read()
+                    page.evaluate(js)
+
             # save as PDF
             pdf = f'{tempdir}/{link.title}.pdf'
             page.pdf(path=pdf)
